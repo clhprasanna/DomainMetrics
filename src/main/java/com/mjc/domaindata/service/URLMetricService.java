64aeefcb6984cc7cc14dce0c8862cdd905939967
clhprasanna <clhprasanna@gmail.com>
2018-12-27 18:08:31 +0530

    code cleanup

--- src/main/java/com/mjc/domaindata/service/URLMetricService.java
+++ src/main/java/com/mjc/domaindata/service/URLMetricService.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,7 +13,6 @@ import java.util.Map;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.hibernate.validator.internal.util.DomainNameUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -23,7 +23,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.mjc.domaindata.MozDomainData.Base64;
 import com.mjc.domaindata.domain.URLMetric;
 import com.mjc.domaindata.domain.URLResult;
 import com.mjc.domaindata.utils.MozUtils;
@@ -32,12 +31,14 @@ import com.mjc.domaindata.utils.MozUtils;
 public class URLMetricService 
 {
 	private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
+	private static final String AccessID = "mozscape-ac52dbcf56";
+	private static final String SecretKey = "bc0cc0d590fce266ba8f9fc26b996439";
 	private Map<String, String> parameters = new HashMap<>();
 	private static final String baseURL = "https://lsapi.seomoz.com/linkscape/url-metrics/";
 
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
 	public URLMetric getURLMetricsForDomain(String domain) throws InvalidKeyException, SignatureException, NoSuchAlgorithmException, URISyntaxException
 	{
 
@@ -45,78 +46,39 @@ public class URLMetricService
 
 		long timeStamp = ((new Date()).getTime())/1000 + expiresInterval;
 		
-		String accessId = "mozscape-ac52dbcf56";
-		String secretKey = "bc0cc0d590fce266ba8f9fc26b996439";
+		// Signature : HMAC-SHA1 hash of AccessID and expires parameter signed using secret key.
+		// The secure hash needs to be Base64 encoded.
 		
-		String signature = calculateHMACSHA1Hash(accessId+String.valueOf(timeStamp),secretKey);
-		
+		String signature = calculateHMACSHA1Hash(AccessID+timeStamp,SecretKey);
 		
 		String finalURL = baseURL + domain;
 		
-		
 		parameters.put("Cols", MozUtils.DomainAuthority_Cols);
-		parameters.put("Limit", "500");
-		parameters.put("AccessID", "mozscape-ac52dbcf56");
+		parameters.put("Limit", "10");
+		parameters.put("AccessID", AccessID);
 		parameters.put("Expires", String.valueOf(timeStamp));
 		parameters.put("Signature", signature);
 		
-		/*
-		
+
 		String urlParams = MozUtils.getParams(parameters);
 		
-		String url = baseURL + "?" + urlParams;
+		finalURL = finalURL + "?" + urlParams;
 		
-		System.out.println(url);
+		// https://lsapi.seomoz.com/linkscape/url-metrics/moz.com?Cols=4&Limit=10&AccessID=mozscape-ac52dbcf56&Expires=1545264000&Signature=bc0cc0d590fce266ba8f9fc26b996439
+		UriComponents uri = UriComponentsBuilder.fromUri(new URI(finalURL)).build();
 		
-		UriComponents uri = UriComponentsBuilder.fromUri(new URI(url)).build();
-		
-		restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor("mozscape-ac52dbcf56", "bc0cc0d590fce266ba8f9fc26b996439"));
+		restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor(AccessID, SecretKey));
 		
 		ResponseEntity<URLResult> response = restTemplate.getForEntity(uri.toUriString(), URLResult.class);
 		
 		URLResult urlResult = response.getBody();
-		
-		*/
-		
-		URLResult urlResult = getResult(finalURL);
 		
 		URLMetric urlMetric = new URLMetric();
 		
 		urlMetric.setDomainAuthority(urlResult.getPda());
 		
-		System.out.println("Domain Authority "+urlResult.getPda());
-		
-		// page authority
-		
-		try {
-			Thread.currentThread().sleep(1000*25);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		parameters.put("Cols", MozUtils.PageAuthority_Cols);
-		urlResult = getResult(finalURL);
-		urlMetric.setPageAuthority(urlResult.getUpa());
-		
-		System.out.println("Page Authority "+urlResult.getUpa());
-		
 		return urlMetric;
 	
-	}
-	
-	private URLResult getResult(String finalURL) throws URISyntaxException
-	{
-		String urlParams = MozUtils.getParams(parameters);
-		String url = finalURL + "?" + urlParams;
-		System.out.println("ffff - "+url);
-		
-		UriComponents uri = UriComponentsBuilder.fromUri(new URI(url)).build();
-		restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor("mozscape-ac52dbcf56", "bc0cc0d590fce266ba8f9fc26b996439"));
-		ResponseEntity<URLResult> response = restTemplate.getForEntity(uri.toUriString(), URLResult.class);
-		
-		URLResult urlResult = response.getBody();
-		return urlResult;
-		
 	}
 	
 	public static String calculateHMACSHA1Hash(String data, String key)
@@ -125,7 +87,8 @@ public class URLMetricService
 		SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), HMAC_SHA1_ALGORITHM);
 		Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
 		mac.init(signingKey);
-		return Base64.encodeBytes(mac.doFinal(data.getBytes()));
+		
+		return new String(Base64.getEncoder().encode(mac.doFinal(data.getBytes())));
 	}
 	
 	@Bean
